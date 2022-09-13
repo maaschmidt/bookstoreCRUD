7194b2eb1a268e3179784f27037bc5d4a54048ab
maaschmidt <marcel.schmidt@universo.univates.br>
2022-09-13 11:17:08 -0300

    update password input

--- views/login/index.js
+++ views/login/index.js
@@ -20,3 +20,21 @@ const userValidate = async () => {
             Swal.fire(` Login error: Email or password are incorrect`)
         });
 }
+
+const togglePassword = document.querySelector("#togglePassword");
+const password = document.querySelector("#password");
+
+togglePassword.addEventListener("click", function () {
+    // toggle the type attribute
+    const type = password.getAttribute("type") === "password" ? "text" : "password";
+    password.setAttribute("type", type);
+
+    // toggle the icon
+    this.classList.toggle("bi-eye");
+});
+
+// prevent form submit
+const form = document.querySelector("form");
+form.addEventListener('submit', function (e) {
+    e.preventDefault();
+});
